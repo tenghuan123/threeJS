a23e59f990f82e797b0f1c29eb24810ceea0aac3
tenghuan123 <2540149177@qq.com>
2021-01-23 23:52:30 +0800

    code update

--- src/pages/webgl_animation_cloth/index.tsx
+++ src/pages/webgl_animation_cloth/index.tsx
@@ -265,7 +265,138 @@ const webGlAnimationCloth = () => {
 
     }
 
+    /* testing cloth simulation  */
 
+    const pinsFormation: (string | any[])[] = [];
+    pins = [ 6 ];
+
+    pinsFormation.push( pins );
+    
+    pins = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+    pinsFormation.push( pins );
+
+    pins = [ 0 ];
+    pinsFormation.push( pins );
+
+    pins = []; //cut the rope ;)
+    pinsFormation.push( pins );
+
+    pins = [ 0, cloth.w ]; // classic 2 pins
+    pinsFormation.push( pins );
+
+    pins = pinsFormation[ 1 ];
+
+    const togglePins = () => {
+        pins = pinsFormation[ ~ ~ ( Math.random() * pinsFormation.length ) ];
+    }
+
+    const init = () => {
+        container = document.createElement( 'div' );
+        document.body.appendChild( container );
+
+        // scene
+
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0xcce0ff );
+        scene.fog = new THREE.Fog( 0xcce0ff, 500, 10000 );
+
+        // camera
+        camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 10000 );
+        camera.position.set( 1000, 50, 1500 );
+
+        // lights
+        scene.add( new THREE.AmbientLight( 0x666666 ) );
+
+        const light = new THREE.DirectionalLight( 0xdfebff, 1 );
+        light.position.set( 50, 200, 200 );
+        light.position.multiplyScalar( 1.3 );
+
+        light.castShadow = true;
+
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
+
+        const d = 300;
+
+        light.shadow.camera.left = - d;
+        light.shadow.camera.right = d;
+        light.shadow.camera.top = d;
+        light.shadow.camera.bottom = -d;
+
+        light.shadow.camera.far = 1000;
+
+        scene.add( light );
+
+        // cloth material
+        const loader = new THREE.TextureLoader();
+        const clothTexture = loader.load( '../../asset/patterns/circuit_pattern.png' );
+        clothTexture.anisotropy = 16;
+
+        const clothMaterial = new THREE.MeshLambertMaterial( { 
+            map: clothTexture,
+            side: THREE.DoubleSide,
+            alphaTest: 0.5
+        } );
+
+        // cloth geometry
+        clothGeometry = new THREE.ParametricBufferGeometry( clothFunction, cloth.w, cloth.h );
+
+        // cloth mesh
+        object = new THREE.Mesh( clothGeometry, clothMaterial );
+        object.position.set( 0, 0, 0);
+        object.castShadow = true;
+        scene.add( object );
+
+        object.customDepthMaterial = new THREE.MeshDepthMaterial( {
+            depthPacking: THREE.RGBADepthPacking,
+            map: clothTexture,
+            alphaTest: 0.5
+        } )
+
+        // sphere
+
+        const ballGeo = new THREE.SphereBufferGeometry( ballSize, 32, 16 );
+        const ballMaterial = new THREE.MeshLambertMaterial();
+
+        sphere = new THREE.Mesh( ballGeo, ballMaterial );
+        sphere.castShadow = true;
+        sphere.receiveShadow = true;
+        sphere.visible = false;
+        scene.add( sphere );
+
+        // ground
+        const groundTexture = loader.load( '../../asset/terrain/grasslight-big.jpg' );
+        groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+        groundTexture.repeat.set( 25, 25 );
+        groundTexture.anisotropy = 16;
+        groundTexture.encoding = THREE.sRGBEncoding;
+
+        const groundMaterial = new THREE.MeshLambertMaterial( { map: groundTexture } );
+
+        let mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 20000, 20000 ), groundMaterial );
+        mesh.position.y = -250;
+        mesh.position.x = - Math.PI / 2;
+        mesh.receiveShadow = true;
+        scene.add( mesh );
+
+        // poles
+        const poleGeo = new THREE.BoxBufferGeometry( 5, 375, 5 );
+        const poleMat = new THREE.MeshLambertMaterial();
+
+        mesh = new THREE.Mesh( poleGeo, poleMat );
+        mesh.position.x = -125;
+        mesh.position.y = - 62;
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        scene.add( mesh );
+
+        mesh = new THREE.Mesh( poleGeo, poleMat );
+        mesh.position.x = 125;
+        mesh.position.y = -62;
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        scene.add( mesh );   
+    }
 
     return <div>Simple Cloth Simulation<br/>
     Verlet integration with relaxed constraints<br/></div>
