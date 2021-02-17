af86bdad0df2fc188f1a95ee754f54791313b1d9
tenghuan123 <2540149177@qq.com>
2021-02-17 21:30:39 +0800

    realize custom geometry

--- src/pages/webgl_animation_cloth/index_study_1.tsx
+++ src/pages/webgl_animation_cloth/index_study_1.tsx
@@ -6,7 +6,8 @@ const webgl_animation_cloth_study1 = () => {
 
     // scence(场景)
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.OrthographicCamera( window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 1000 )
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     const Element = document.getElementById('three-canvas');
@@ -17,10 +18,56 @@ const webgl_animation_cloth_study1 = () => {
     }
 
     // 物体
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00} );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    // 立体矩形
+    const getBoxGeometry = () => {
+        // 创建立方体 网格体 没有单位
+        const BoxGeometry = new THREE.BoxGeometry( 1, 2, 3, 1, 2, 3 );
+        // 根据分段情况，创建网格体
+        const frameGeometry = new THREE.WireframeGeometry( BoxGeometry );
+        // 根据网格，生成线条
+        const line = new THREE.LineSegments( frameGeometry );
+        // 基础简单材质
+        const mateial = new THREE.MeshNormalMaterial();
+        const mesh = new THREE.Mesh( BoxGeometry, mateial );
+        return line;
+    }
+    // 直角坐标系
+    const getAxesHelper = () => {
+        return new THREE.AxesHelper(5);
+    }
+
+    // 随机三个点形成一个面
+    const getPlane = () => {
+        // 生成对应的三个点
+        const geometry = new THREE.Geometry();
+
+        geometry.vertices.push(new THREE.Vector3( 1, 0, 0 ))
+        geometry.vertices.push(new THREE.Vector3( 0, 1, 0 ))
+        geometry.vertices.push(new THREE.Vector3( 0, 0, 1 ))
+        geometry.vertices.push(new THREE.Vector3( 1, 1, 1 ))
+        console.log(geometry);
+        const normal = new THREE.Vector3( 0, 1, 0 )
+        const color = new THREE.Color( 0xff00ff )
+        // 生成了一个面，并且添加到模型中
+        geometry.faces.push(new THREE.Face3( 0, 1, 2, normal, color ))
+        geometry.faces.push(new THREE.Face3( 3, 1, 2, normal, color ))
+        geometry.faces.push(new THREE.Face3( 0, 3, 2, normal, color ))
+        geometry.faces.push(new THREE.Face3( 0, 1, 3, normal, color ))
+        const Materials = new THREE.MeshNormalMaterial({
+            side: THREE.DoubleSide
+        })
+
+        // geometry.faces
+        return new THREE.Mesh( geometry, Materials );
+    }
+
+    const axesHelper = getAxesHelper();
+    const cube = getBoxGeometry();
+    const onePlane = getPlane();
+    
+    scene.add( onePlane );
+    // scene.add( cube );
+    scene.add( axesHelper );
 
     camera.position.z = 5;
 
