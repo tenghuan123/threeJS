1aa77741494cd3050fdc21488734c14bba6f51c7
tenghuan123 <2540149177@qq.com>
2021-03-09 21:57:52 +0800

    loader gltf file

--- src/pages/webgl_animation_cloth/loaderObject.tsx
+++ src/pages/webgl_animation_cloth/loaderObject.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from 'three'
 import SelfThreeBase from '../SelfThreeBase';
+// import DamagedHelmet from '@/module/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf'
 
 interface loaderThreeObjectProps {
     queryElement:string;
@@ -10,12 +12,22 @@ class loaderThreeObject extends SelfThreeBase {
     constructor(props:loaderThreeObjectProps){
         super(props);
         this.loaderObj();
+        this.addLight();
     }
     loaderObj() {
         const gltfLoader = new GLTFLoader();
-        gltfLoader.load("@/module/gltf/DamagedHelmet/glTF/DamagedHelmet",(res)=>{
-            console.log(res);
+        gltfLoader.load('/module/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf',(res)=>{
+            this.scence.add(res.scene);
+            this.scence.traverse(mesh => {
+                if(mesh.isMesh){
+                    mesh.material.side = THREE.DoubleSide;
+                   mesh.material.envMap = this.scence.background; 
+                }
+            })
         },(event)=>{console.log(event)},(Err)=>{console.log(Err)});
+    }
+    addLight() {
+        this.scence.add(new THREE.AmbientLight(0xffffff,0.7))
     }
 }
 
